2824d18cd6c0062eb84ddd5f4587b205566f9d63
Reknij <jinker520@gmail.com>
2024-05-13 12:27:02 +0800

    update to latest and optimizee size

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ The plugin is used to resolve the title of youtube music file downloaded by [yt-
 Now working resolve:
 - remove the video id from title.
 - set the cover image by video id.
+
+# Build
+```
+rustup target add wasm32-wasi
+
+cargo build --target wasm32-wasi --release
+```
+
+Copy the target `wasm` file to diosic `<diosic-data-path>/plugins/diosic-plugin-youtube/main.wasm`. (Create directory if not exists.)
--- src/lib.rs
+++ src/lib.rs
@@ -1,69 +1,64 @@
-use regex::Regex;
+use lazy_static::lazy_static;
+use regex_lite::Regex;
 use serde::{Deserialize, Serialize};
-use std::{ffi::{CStr, CString}, mem};
+use std::{mem, path::PathBuf};
+
+extern "C" {
+    fn callback(info_ptr: *mut u8, info_len: u32);
+}
+
+lazy_static! {
+    static ref YOUTUBE_MEDIA_IDENT: Regex = Regex::new(r"\s\[([a-zA-Z0-9_-]+)\]$").unwrap();
+}
 
 #[no_mangle]
-pub extern "C" fn process_media_info_json(info_ptr: *const u8, file_path: *const u8) -> *const u8 {
-    let str = unsafe { CStr::from_ptr(info_ptr as _).to_str().unwrap() };
-    let info = serde_json::from_str::<MediaInfo>(str);
-    match info {
+pub extern "C" fn process_media_info_json(info_ptr: *mut u8, info_len: u32) {
+    let info_len = info_len as usize;
+    let buffer = unsafe { Vec::from_raw_parts(info_ptr, info_len, info_len) };
+    match serde_json::from_slice::<MediaInfo>(&buffer) {
         Ok(mut info) => {
-            let re = Regex::new(r"\s\[([a-zA-Z0-9_-]+)\]$").unwrap();
             let title = info.title.clone();
-            let caps = re.captures(&title);
-            match caps {
-                Some(caps) => {
-                    let vid = &caps[1];
-                    if info.title != vid {
-                        info.title = info.title.replace(&format!(" [{}]", vid), "");
-                        info.categories.push("YouTube".to_owned());
-                        info.cover = Some(format!("http://img.youtube.com/vi/{}/hqdefault.jpg", vid));
-
-                        let r = serde_json::to_string(&info);
-                        if let Ok(r) = r {
-                            let c_string = CString::new(r);
-                            match c_string {
-                                Ok(cs) => {
-                                    let ptr = cs.as_ptr();
-                                    mem::forget(cs);
-                                    ptr as _
-                                },
-                                Err(_) => info_ptr,
-                            }
-                        } else {
-                            info_ptr
-                        }
-                    }
-                    else {
-                        info_ptr
+            let caps = YOUTUBE_MEDIA_IDENT.captures(&title);
+            if let Some(caps) = caps {
+                let vid = &caps[1];
+                if info.title != vid {
+                    info.title = info.title.replace(&format!(" [{}]", vid), "");
+                    info.categories.push("YouTube".to_owned());
+                    info.cover_url = Some(format!("https://i3.ytimg.com/vi/{vid}/mqdefault.jpg"));
+                    if let Ok(mut bytes) = serde_json::to_string(&info).map(|r| r.into_bytes()) {
+                        let ptr = bytes.as_mut_ptr();
+                        let len = bytes.len();
+                        mem::forget(bytes);
+                        unsafe { callback(ptr, len as u32) };
                     }
                 }
-                None => info_ptr,
             }
         }
         Err(err) => {
-            println!("err: {}", err);
-            info_ptr
+            println!("Parse media info error: {}", err);
         }
     }
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct MediaInfo {
-    pub id: String,
-    pub path: String,
+    pub id: i64,
+    pub path: PathBuf,
     pub title: String,
     pub album: String,
     pub artist: String,
     pub genre: String,
-    pub year: String,
+    pub year: u32,
     pub library: String,
-    pub cover: Option<String>,
-    pub categories: Vec<String>,
-    pub simple_rate: Option<u32>,
+    pub cover_path: Option<PathBuf>,
+    pub cover_url: Option<String>,
+    pub sample_rate: Option<u32>,
     pub bit_depth: Option<u8>,
     pub audio_bitrate: Option<u32>,
     pub overall_bitrate: Option<u32>,
     pub channels: Option<u8>,
-    pub duration_milliseconds: u128,
+    pub duration_seconds: u32,
+    pub categories: Vec<String>,
+    pub file_name: String,
+    pub file_type: String,
 }
